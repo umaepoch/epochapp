71b12bad109ffdbde47c891fc31c1e2d510dcb03
Uma <umag@epochconsulting.in>
2016-09-18 14:41:42 +0530

    Reports

---
+++ epochapp/epochapp/report/stock_ledger_modified/stock_ledger_modified.js
@@ -0,0 +1,60 @@
+// Copyright (c) 2016, Epoch and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Stock Ledger Modified"] = {
+	"filters": [
+
+	        {
+                        "fieldname":"company",
+                        "label": __("Company"),
+                        "fieldtype": "Link",
+                        "options": "Company",
+                        "default": frappe.defaults.get_user_default("Company"),
+                        "reqd": 1
+                },
+                {
+                        "fieldname":"from_date",
+                        "label": __("From Date"),
+                        "fieldtype": "Date",
+                        "default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+                        "reqd": 1
+                },
+                {
+                        "fieldname":"to_date",
+                        "label": __("To Date"),
+                        "fieldtype": "Date",
+                        "default": frappe.datetime.get_today(),
+                        "reqd": 1
+                },
+                {
+                        "fieldname":"warehouse",
+                        "label": __("Warehouse"),
+                        "fieldtype": "Link",
+                        "options": "Warehouse"
+                },
+                {
+                        "fieldname":"item_code",
+                        "label": __("Item"),
+                        "fieldtype": "Link",
+                        "options": "Item"
+                },
+                {
+                        "fieldname":"brand",
+                        "label": __("Brand"),
+                        "fieldtype": "Link",
+                        "options": "Brand"
+                },
+                {
+                        "fieldname":"voucher_no",
+                        "label": __("Voucher #"),
+                        "fieldtype": "Data"
+                }
+        ]
+}
+
+// $(function() {
+//      $(wrapper).bind("show", function() {
+//              frappe.query_report.load();
+//      });
+// });
+
