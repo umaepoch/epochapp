1e8e27fc360ecd0cdaaf813435c70c46d080bfc0
Uma <umag@epochconsulting.in>
2016-12-21 13:17:32 +0530

    Report

--- epochapp/epochapp/report/so_full_with_delivery_note/so_full_with_delivery_note.py
+++ epochapp/epochapp/report/so_full_with_delivery_note/so_full_with_delivery_note.py
@@ -60,11 +60,9 @@ def execute(filters=None):
 				diff_days = rows[3] - rows[2]
 
 			if flt(diff_days) <= 0 and rows[3] != temp_date:
-				msgprint(_("Inside 1"))
-				msgprint(_(item_del_qty))
-				msgprint(_(tot_si_qty))
+				
 				per_qty = (item_del_qty / tot_si_qty) * 100
-				msgprint(_(per_qty))
+				
 			summ_data.append([rows[13], " ", order_prev, rows[1], rows[2],
 			 	rows[3], diff_days, rows[4], rows[5], rows[6], 
 				rows[7], rows[8], rows[9], rows[10], rows[11], item_pend_qty, per_qty
@@ -98,11 +96,9 @@ def execute(filters=None):
 				tot_pend_qty = tot_si_qty - tot_del_qty
 
 				if flt(diff_days) <= 0 and rows[3] != temp_date:
-					msgprint(_("Inside 2"))
-					msgprint(_(item_del_qty))
-					msgprint(_(rows[9]))
+					
 					per_qty = (item_del_qty / rows[9]) * 100
-					msgprint(_(per_qty))
+		
 				summ_data.append([rows[13], " ", order_prev, rows[1], rows[2],
 			 	rows[3], diff_days, rows[4], rows[5], rows[6], 
 				rows[7], rows[8], rows[9], rows[10], rows[11], item_pend_qty, per_qty
@@ -123,11 +119,9 @@ def execute(filters=None):
 				item_del_qty = rows[11]		 	 
 				item_pend_qty = rows[9] - rows[11] - item_pend_qty
 				if flt(diff_days) <= 0 and rows[3] != temp_date:
-					msgprint(_("Inside 3"))
-					msgprint(_(item_del_qty))
-					msgprint(_(tot_si_qty))					
+									
 					per_qty = (item_del_qty / tot_si_qty) * 100
-					msgprint(_(per_qty))
+					
 
 				summ_data.append([rows[13], " ", order_work, rows[1], rows[2],
 			 	rows[3], diff_days, rows[4], rows[5], rows[6], 
@@ -139,11 +133,9 @@ def execute(filters=None):
                                 item_prev = item_work
 
 				if flt(diff_days) <= 0 and rows[3] != temp_date:
-					msgprint(_("Inside 4"))	
-					msgprint(_(item_del_qty))
-					msgprint(_(tot_si_qty))
+					
 					per_qty = (item_del_qty / tot_si_qty) * 100
-					msgprint(_(per_qty))
+					
 
 		order_count = order_count + 1 
 	summ_data.append([" ", " ", order_prev, " ", 
